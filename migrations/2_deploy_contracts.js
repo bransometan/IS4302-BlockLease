612385614eafecfc9186631e404de76937804543
michaelseyo <76872115+michaelseyo@users.noreply.github.com>
2024-04-06 23:33:51 +0800

    add deployment of contract and build for use on client

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,25 @@
-const Dice = artifacts.require("Dice");
-const DiceBattle = artifacts.require("DiceBattle");
+const LeaseToken = artifacts.require("LeaseToken");
+const PaymentEscrow = artifacts.require("PaymentEscrow");
+const RentalProperty = artifacts.require("RentalProperty");
+const RentalMarketplace = artifacts.require("RentalMarketplace");
+
+const PROTECTION_FEE = 1;
+const COMMISSION_FEE = 1;
 
 module.exports = (deployer, network, accounts) => {
-    deployer.deploy(Dice).then(function() {
-        return deployer.deploy(DiceBattle, Dice.address);
-    })
-}
+  deployer.deploy(LeaseToken).then(function () {
+    return deployer.deploy(
+      PaymentEscrow,
+      LeaseToken.address,
+      PROTECTION_FEE,
+      COMMISSION_FEE
+    );
+  });
+  deployer.deploy(RentalProperty).then(function () {
+    return deployer.deploy(
+      RentalMarketplace,
+      RentalProperty.address,
+      PaymentEscrow.address
+    );
+  });
+};
