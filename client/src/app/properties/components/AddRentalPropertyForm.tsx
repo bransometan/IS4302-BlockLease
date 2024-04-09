2126b8417c77db14f930088dfb46b0fa441baf72
michaelseyo <76872115+michaelseyo@users.noreply.github.com>
2024-04-09 17:00:24 +0800

    base integration of add and get rental property

--- client/src/app/properties/components/AddRentalPropertyForm.tsx
+++ client/src/app/properties/components/AddRentalPropertyForm.tsx
@@ -13,35 +13,95 @@ import {
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useToast } from "@/components/ui/use-toast";
+import { addRentalProperty } from "@/services/rentalProperty";
+import { PropertyType } from "@/types/contracts";
+import { RootState } from "@/types/state";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useSelector } from "react-redux";
 import { z } from "zod";
 
 const formSchema = z.object({
   location: z.string().min(1, "Location must be at least 1 character"),
+  postalCode: z.string().length(6, "Postal code must be 6 characters"),
+  unitNumber: z.string().min(1, "Unit number must be at least 1 character"),
+  propertyType: z.nativeEnum(PropertyType),
+  description: z.string().min(1, "Description must be at least 1 character"),
+  numOfTenants: z.coerce
+    .number()
+    .positive("Number of tenants must be greater than 0"),
+  rentalPrice: z.coerce
+    .number()
+    .positive("Rental price must be greater than 0"),
+  leaseDuration: z.coerce
+    .number()
+    .positive("Lease duration (months) must be greater than 0"),
 });
 
 export default function AddRentalPropertyForm() {
+  const { toast } = useToast();
+  const { wallet } = useSelector((states: RootState) => states.globalStates);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {},
+    defaultValues: {
+      location: "",
+      postalCode: "",
+      unitNumber: "",
+      propertyType: PropertyType.HDB,
+      description: "",
+      numOfTenants: 1,
+      rentalPrice: 1,
+      leaseDuration: 12,
+    },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {}
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!wallet) {
+      toast({
+        description: "Please connect to your wallet!",
+      });
+      return;
+    }
+    try {
+      await addRentalProperty(values);
+      toast({
+        title: "Success!",
+        description: "Rental property successfully added",
+      });
+      form.reset();
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Oops",
+        description: "Something went wrong",
+      });
+      console.error(error);
+    }
+  }
 
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button>Add Rental Property</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[600px]">
+      <DialogContent className="max-w-[600px] max-h-[500px] overflow-scroll">
         <DialogHeader>
           <DialogTitle>Add Rental Property</DialogTitle>
           <DialogDescription>
@@ -61,6 +121,126 @@ export default function AddRentalPropertyForm() {
                   <FormLabel>Location</FormLabel>
                   <FormControl>
                     <Input placeholder="Enter location" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="postalCode"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Postal Code</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter postal code" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="unitNumber"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Unit Number</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter unit number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="propertyType"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Property Type</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a property type" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {Object.values(PropertyType).map((propertyType, i) => {
+                        return (
+                          <SelectItem key={i} value={propertyType}>
+                            {propertyType}
+                          </SelectItem>
+                        );
+                      })}
+                    </SelectContent>
+                    <FormMessage />
+                  </Select>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter Description" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="numOfTenants"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Number of tenants</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter number of tenants"
+                      {...field}
+                      type="number"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="rentalPrice"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Rental Price</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter rental price"
+                      {...field}
+                      type="number"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="leaseDuration"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Lease duration</FormLabel>
+                  <FormDescription>In Months</FormDescription>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter lease duration"
+                      {...field}
+                      type="number"
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
