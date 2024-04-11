dba7f781f407f82119fb3023a6bf59400daa2dc1
brannn99 <bransometan@gmail.com>
2024-04-11 22:04:05 +0800

    update deploy contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,12 @@ const LeaseToken = artifacts.require("LeaseToken");
 const PaymentEscrow = artifacts.require("PaymentEscrow");
 const RentalProperty = artifacts.require("RentalProperty");
 const RentalMarketplace = artifacts.require("RentalMarketplace");
+const RentDisputeDAO = artifacts.require("RentDisputeDAO");
 
 const PROTECTION_FEE = 50;
 const VOTER_REWARD = 50;
+const VOTE_PRICE = 1;
+const MINIMUM_VOTES = 3;
 
 module.exports = (deployer, network, accounts) => {
   deployer.deploy(LeaseToken).then(function () {
@@ -12,7 +15,8 @@ module.exports = (deployer, network, accounts) => {
       PaymentEscrow,
       LeaseToken.address,
       PROTECTION_FEE,
-      VOTER_REWARD
+      VOTER_REWARD,
+      VOTE_PRICE
     );
   });
   deployer.deploy(RentalProperty).then(function () {
@@ -22,4 +26,5 @@ module.exports = (deployer, network, accounts) => {
       PaymentEscrow.address
     );
   });
+  deployer.deploy(RentDisputeDAO, RentalProperty.address, PaymentEscrow.address, RentalMarketplace.address, MINIMUM_VOTES);
 };
